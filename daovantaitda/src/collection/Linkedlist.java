d91f5096a76ba6b96dd87d1cd6d13e794272927c
daovantai <daotai2309@gmail.com>
2022-11-23 21:44:49 +0700

    test

---
+++ daovantaitda/src/collection/Linkedlist.java
@@ -0,0 +1,7 @@
+package collection;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
--- tdadaovantai/src/collection/test.java
+++
@@ -1,9 +0,0 @@
-package collection;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hhhh");
-        System.out.println("oooo");
-        System.out.println("ngay4");
-    }
-}
