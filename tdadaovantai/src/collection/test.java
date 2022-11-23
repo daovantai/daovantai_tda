bfdcf5d962a53fe300a8e26d83883b5558952934
daovantai <daotai2309@gmail.com>
2022-11-23 20:54:50 +0700

    day4

--- tdadaovantai/src/collection/test.java
+++ tdadaovantai/src/collection/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hhhh");
         System.out.println("oooo");
+        System.out.println("ngay4");
     }
 }
