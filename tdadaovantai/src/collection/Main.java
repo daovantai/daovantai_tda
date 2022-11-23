1c20a0003824021bbf040edb1af59a3403a37f97
daovantai <daotai2309@gmail.com>
2022-11-23 20:24:24 +0700

    day2

--- tdadaovantai/src/collection/Main.java
+++ tdadaovantai/src/collection/Main.java
@@ -3,5 +3,8 @@ package collection;
 public class Main {
     public static void main(String[] args) {
         System.out.println("tb");
+        System.out.println("okla");
+        System.out.println("cc");
+        System.out.println("bb");
     }
 }
